b69bedc0c1c458974f2c80354c3b3aef431937c0
jiapeng <jiapeng.chen@gezbox.com>
2014-04-10 11:59:20 +0800

    config scripts in setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup
 setup(
     name="django-column-comment",
     version="0.0.1",
-    py_modules=["django-column-comment"],
+    py_modules=["dcc"],
     author="Altynai",
+    scripts=["dcc.py"],
     author_email="Altynai.me@gmail.com",
     url="http://Altynai.me",
     description="dump column comment",
